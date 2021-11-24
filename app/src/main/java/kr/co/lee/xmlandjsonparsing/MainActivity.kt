28acc612c27b73fb04e2c3bb7986c55def606ab5
dhtmaks2540 <gjsxj725@daum.net>
2021-11-24 10:41:50 +0900

    Example...

--- app/src/main/java/kr/co/lee/xmlandjsonparsing/MainActivity.kt
+++ app/src/main/java/kr/co/lee/xmlandjsonparsing/MainActivity.kt
@@ -22,12 +22,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // view 객체 초기화
         pullBtn = findViewById(R.id.pull_btn)
         jsonBtn = findViewById(R.id.json_btn)
         resultView = findViewById(R.id.result_title)
         cityView = findViewById(R.id.city_view)
         temperatureView = findViewById(R.id.temperature_view)
 
+        // 버튼 클릭 리스너
         pullBtn.setOnClickListener {
             pullParsing()
         }
@@ -52,10 +54,9 @@ class MainActivity : AppCompatActivity() {
             // getEventType() 메서드를 사용해 이벤트 타입을 얻어오는데
             // 처음으로 호출되므로 START_DOCUMENT가 호출
             var eventType = parser.eventType
-            var done = false
 
             // eventType이 End_Document가 아니고 done이 false일 경우 반복
-            while(eventType != XmlPullParser.END_DOCUMENT && !done) {
+            while(eventType != XmlPullParser.END_DOCUMENT) {
                 // 이름을 저장하기 위한 변수
                 var name: String? = null
                 // eventType이 START_TAG인 경우
@@ -81,22 +82,35 @@ class MainActivity : AppCompatActivity() {
 
     private fun jsonParsing() {
         resultView.text = "JSON Parsing Result"
-        // 파일 읽기
+        // Json을 저장하기 위한 변수
         var json: String? = null
 
         try {
+            // assets 폴더안에 있는 JSON 파일을 열어 InputStream 객체 생성
             val inputStream = assets.open("test.json")
+            // 한번에 읽을 수 있는 사이즈
             val size = inputStream.available()
+            // Byte 배열 생성
             val buffer = ByteArray(size)
+            // InputStream으로 부터 들어온 데이터 읽고 buffer에 저장
             inputStream.read(buffer)
+            // InputStream close
             inputStream.close()
+            // String으로 변환
             val json = String(buffer, charset("UTF-8"))
 
+            // JSONObject 객체 생성(최상위 JSON 객체)
             val jsonRoot = JSONObject(json)
 
+            // JSONArray 획득
+            val jsonArray = jsonRoot.getJSONArray("weather")
+
+            // jsonRoot 객체 하위에 데이터 획득
             cityView.text = jsonRoot.getString("name")
-            val main = jsonRoot.getJSONObject("main")
-            temperatureView.text = main.getString("temp")
+            // jsonRoot 객체 하위에 JSONObject 획득
+            val jsonMain = jsonRoot.getJSONObject("main")
+            // jsonMain 객체 하위에 데이터 획득
+            temperatureView.text = jsonMain.getString("temp")
         } catch(e: Exception) {
             e.printStackTrace()
         }
